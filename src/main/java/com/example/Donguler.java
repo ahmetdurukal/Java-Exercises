394fa5758e061c6a9cfb91f6f180fba694e66d5e
Ahmetdrkl <ahmetmahmutdurukal@gmail.com>
2023-05-20 19:36:09 +0300

    initial commit

---
+++ src/main/java/com/example/Donguler.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class Donguler {
+    public static void main(String[] args) {
+        //For
+        for (int a = 1; a <= 10; a = a+1) {
+            System.out.println(a);
+        }
+        System.out.println("For Döngüsü bitti");
+        //While
+        int i=0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While döngüsü bitti");
+        //Do while
+        int j = 1;
+        do {
+            System.out.println(j);
+            j=j+2;
+        }
+        while(j<10);
+        System.out.println("Do-while döngüsü bitti");
+        }
+    }
---
+++ src/main/java/com/example/Test.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci Sayım: ";
+        System.out.println(mesaj + ogrenciSayisi);
+        int sayi = 24;
+        if (sayi < 20) {
+            System.out.println("Sayı 20'den küçüktür.");
+        } else if (sayi == 20) {
+            System.out.println("Sayı 20'ye eşittir.");
+        } else {
+            System.out.println("Sayı 20'den büyüktür");
+        }
+    }
+}
+
