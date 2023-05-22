24620d38a03d7344ce3e70f23a0538aeeed81e27
Ahmetdrkl <ahmetmahmutdurukal@gmail.com>
2023-05-22 13:15:36 +0300

    Prime Number

---
+++ src/main/java/com/example/AsalSayi.java
@@ -0,0 +1,33 @@
+package com.example;
+public class AsalSayi {
+    public static void main(String[] args){
+        int number =7;
+        int remainder = number % 2;
+        //System.out.println(remainder);
+        boolean isPrime = true;
+
+        if(number==1){
+            System.out.println("Asal Değildir");
+            return;
+        }
+
+        if(number<1){
+            System.out.println("Geçersiz Sayi");
+            return;
+        }
+
+        for (int i=2; i<number; i++){
+            if (number % i == 0){
+                isPrime = false;
+
+            }
+        }
+        if(isPrime==true){
+            System.out.println("Sayi Asaldır.");
+        }else{
+            System.out.println("Asal Değildir");
+        }
+
+
+    }
+}
