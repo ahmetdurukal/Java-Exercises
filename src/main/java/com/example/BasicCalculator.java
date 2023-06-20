8f474e21cbf1d4b768a4f2a3420a88a3bb0753c0
Ahmetdrkl <ahmetmahmutdurukal@gmail.com>
2023-06-21 01:26:43 +0300

    Method Overloading

--- src/main/java/com/example/BasicCalculator.java
+++ src/main/java/com/example/BasicCalculator.java
@@ -4,6 +4,9 @@ public class BasicCalculator {
     public int topla(int sayi1, int sayi2) {
         return sayi1 + sayi2;
     }
+    public int topla(int sayi1, int sayi2,int sayi3) {
+        return sayi1 + sayi2 + sayi3;
+    }
     public int cikar(int sayi1, int sayi2) {
         return sayi1 - sayi2;
     }
@@ -19,10 +22,11 @@ public class BasicCalculator {
     public static void main(String[] args) {
         int answer = 0;
         BasicCalculator BasicCalculator = new BasicCalculator();
-        answer = BasicCalculator.topla(8, 2);
+        System.out.println(answer = BasicCalculator.topla(8, 2));
+        System.out.println(answer = BasicCalculator.topla(5,7,9));
         // answer = BasicCalculator.cikar(8, 2);
         //answer=BasicCalculator.carp(8,2);
         //answer=BasicCalculator.bol(8,2);
-        System.out.println(answer);
+
     }
 }
